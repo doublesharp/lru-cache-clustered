beb606cdb0c21756643c4d83652cb87958571821
doublesharp <jsilver@doublesharp.com>
2026-04-27 17:45:32 -0700

    chore: add tsconfig and tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  target: 'node22',
+  outDir: 'dist',
+  splitting: false,
+  shims: false,
+  cjsInterop: true,
+});
